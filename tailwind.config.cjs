7fef81c082c75de94f9abe0f1614a3cba0ab6209
VinBHuynh <duybaohuynh1312@gmail.com>
2022-11-20 04:12:20 +0000

    Add Brief page and outline

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'custom': ['Poppins', 'sans-serif']
+    },
   },
   plugins: [],
 };
